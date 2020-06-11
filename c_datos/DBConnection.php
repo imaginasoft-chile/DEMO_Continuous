4e489e7a1fb57152ef9c8f3dc1dacfcf9ca2d830
David Olivares <53587080+dolivaresIS@users.noreply.github.com>
2020-06-11 09:49:44 -0400

    Mejora conexion

--- c_datos/DBConnection.php
+++ c_datos/DBConnection.php
@@ -4,7 +4,7 @@ class DBConnection
 {
 	function Connect(){
 		try {
-		$mycnx =  new PDO('mysql:host=localhost;port=3308;dbname=pv_empmaruri', 'root', 'root');
+		$mycnx =  new PDO('mysql:host=localhost;port=3306;dbname=pv_empmaruri', 'root', 'root');
 				$mycnx->exec("set names utf8");
 				return ($mycnx);
 			} catch (PDOException $e) {
@@ -15,7 +15,7 @@ class DBConnection
 	
 	function ConnectToMasterBD(){
 		try {
-			$mycnx =  new PDO('mysql:host=localhost;port=3308;dbname=cyclo_admin', 'root', 'root');
+			$mycnx =  new PDO('mysql:host=localhost;port=3306;dbname=cyclo_admin', 'root', 'root');
 			$mycnx->exec("set names utf8");
 			return ($mycnx);
 		} catch (PDOException $e) {
